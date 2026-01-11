564d1635c01c1206e0d7db89fd22d85f44a28bd0
alderliesteben <alderliesteben@gmail.com>
2026-01-11 13:56:04 +1000

    Just testing

---
+++ testing/page.tsx
@@ -0,0 +1,26 @@
+export default function Page() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#16a34a", // green
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "24px",
+      }}
+    >
+      <h1
+        style={{
+          margin: 0,
+          fontSize: "64px",
+          fontFamily: '"Brush Script MT", "Segoe Script", cursive',
+          color: "white",
+          lineHeight: 1,
+        }}
+      >
+        Ben
+      </h1>
+    </main>
+  );
+}
